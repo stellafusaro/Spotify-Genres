20d64e150f1c4c98412b71068ca7d6976fef25f9
stellafusaro <143945124+stellafusaro@users.noreply.github.com>
2023-10-20 16:53:19 +0200

    Update Spotify.py

--- Spotify.py
+++ Spotify.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[42]:
-
-
 import requests
 import json
 import random
@@ -11,20 +5,17 @@ import string
 from urllib.parse import urlencode
 import base64
 import webbrowser
+
+# Your Spotify API credentials
 client_id = 'your client id'
 client_secret = 'your client secret'
 
-
-
 scope = "playlist-modify-private"
-
-
-request_url = "https://accounts.spotify.com/authorize?"
-redirect_uri = 'http://localhost:3000';
-scope = 'playlist-modify-private';
+redirect_uri = 'http://localhost:3000'
+scope = 'playlist-modify-private'
 
 data = {
-    'client_id' : client_id,
+    'client_id': client_id,
     'response_type': 'code',
     'redirect_uri': redirect_uri,
     'scope': scope
@@ -32,18 +23,12 @@ data = {
 
 webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(data))
 
-#print(user_response)
-
+# Code retrieval and authorization
 
 # In[43]:
-
-
 code = 'your code'
 
-
 # In[44]:
-
-
 auth_url = 'https://accounts.spotify.com/api/token'
 
 data = {
@@ -56,86 +41,39 @@ data = {
     'content-type': 'application/x-www-form-urlencoded',
 }
 
-auth_response = requests.post(url = auth_url, data=data)
+auth_response = requests.post(url=auth_url, data=data)
 access_token = auth_response.json().get('access_token')
 print(auth_response.json())
 
-
-# In[4]:
-
-
-
-
+# Functions for genre searching
 
 # In[34]:
-
-
-import requests
-
-# Replace with your Spotify artist ID and access token
-artist_id = "15kQGEy89K8deJcZVFEn0N"
-#access_token = "BQBZ9TJRa84D2QI2gBnAD0glcL1qSm343ZWwEbJ8TD6iPz_KHQFJKKb5VRsBl7mG2VNJE1UqAB1K-pMRfaiWzEag5BZ9SoBjxtZ54GVKgqUSGy70s9gSS68pRUhCZ9kfQbGd863o9Zd9izj9RrfWC7OsAG-Xiy7QKJvAAC-TKm_3-IJdKybO5MjU03yz-ASP9lRIWrb9UEEHwYw"
-
-# Define the endpoint URL for the Spotify API
-endpoint_url = f"https://api.spotify.com/v1/artists/{artist_id}"
-
-# Set up headers with the authorization token
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-
-# Make a GET request to retrieve artist information
-response = requests.get(url=endpoint_url, headers=headers)
-
-# Check if the request was successful (status code 200)
-#if response.status_code == 200:
-artist_info = response.json()
-    
-    # Print the artist information
-print("Artist Name:", artist_info['name'])
-print("Followers:", artist_info['followers']['total'])
-print("Genres:", ", ".join(artist_info['genres']))
-print("Popularity:", artist_info['popularity'])
-    # Add more fields as needed
-#else:
-   # print("Error:", response.status_code)
-
-
-# In[35]:
-
 
 import requests
 from requests import get
 
 
-
-def search_for_genres(token,artist_name):
+def search_for_genres(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = {
-    "Authorization": f"Bearer {token}"
+        "Authorization": f"Bearer {token}"
     }
     query = f"q={artist_name}&type=artist&limit=1"
-    
+
     query_url = url + "?" + query
-    result = get(query_url,headers=headers)
+    result = get(query_url, headers=headers)
     json_result = result.json()
     artists = json_result.get("artists", {}).get("items", [])
     for artist in artists:
         genres = artist.get("genres", [])
-        print("Genres:", ", ".join(genres))
-    
-search_for_generes(access_token, "ACDC")
+        print("Genres:", ", ".join(genres)
 
 
-# In[36]:
+search_for_genres(access_token, "ACDC")
 
-
-"https://api.spotify.com/v1/me/top/tracks?limit=10&offset=0"
-
+# Process data and create playlists
 
 # In[37]:
-
-
 import json
 import matplotlib.pyplot as plt
 import datetime as dt
@@ -143,49 +81,47 @@ import numpy as np
 import pandas as pd
 import os
 
-folder_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'Computer Science and Math', 'Spotify Project','MyData')
-#folder_path = '/home/Stella Fusaro/Documents/path/to/Computer Science and Math/Spotify Project/MyData'
-
-json_files =[f for f in os.listdir(folder_path)if f.endswith('.json')]
+folder_path = os.path.join(os.path.expanduser('~'), 'your folder')
+json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
 
 all_data = pd.DataFrame()
 
 for json_file in json_files:
-    with open(os.path.join(folder_path,json_file), 'r')as file:
+    with open(os.path.join(folder_path, json_file), 'r') as file:
         data = json.load(file)
         temporary = pd.DataFrame(data)
-        all_data = pd.concat([all_data, temporary],ignore_index = True)
+        all_data = pd.concat([all_data, temporary], ignore_index=True)
 
-all_data.drop(['username','platform','user_agent_decrypted','episode_name','episode_show_name','spotify_episode_uri','shuffle','offline','offline_timestamp','incognito_mode'],axis=1,inplace = True)
-#print(all_data)
-
+all_data.drop(['username', 'platform', 'user_agent_decrypted', 'episode_name', 'episode_show_name', 'spotify_episode_uri', 'shuffle', 'offline', 'offline_timestamp', 'incognito_mode'], axis=1, inplace=True)
 print("Shape of the DataFrame:", all_data.shape)
 n = 20
 test_data = all_data.head(n)
 
 songs = {}
 
-for index,row in all_data.iterrows():
+for index, row in all_data.iterrows():
     song = row['master_metadata_track_name']
     artist = row['master_metadata_album_artist_name']
     uri = row['spotify_track_uri']
-    
+
     if song and artist:
-        if(song,artist) not in songs.items():
+        if (song, artist) not in songs.items():
             songs[song] = {'artist': artist, "uri": uri}
-        
+
 print(songs)
 print('hi')
 
-keyList = ["pop", "rap", "rock", "classical","country", "soul", "techno", "italian", "soundtrack", "alternative", "latin", "french"]
+keyList = ["pop", "rap", "rock", "classical", "country", "soul", "techno", "italian", "soundtrack", "alternative", "latin", "french"]
 
 the_genres = {key: [] for key in keyList}
 artists_processed = set()
 
+
 def genre_contains_keyword(genre, keyword):
     return keyword.lower() in genre.lower()
 
-def search_for_genres(token, artist_name,uri):
+
+def search_for_genres(token, artist_name, uri):
     print(f"Searching for genres for {artist_name}")
     url = "https://api.spotify.com/v1/search"
     headers = {
@@ -202,7 +138,7 @@ def search_for_genres(token, artist_name,uri):
         if artist_name not in artists_processed:
             genres = artist.get("genres", [])
             if genres:
-                for genre in genres: 
+                for genre in genres:
                     for keyword in keyList:
                         if genre_contains_keyword(genre, keyword):
                             if uri not in the_genres[keyword]:
@@ -210,25 +146,24 @@ def search_for_genres(token, artist_name,uri):
                                 the_genres[keyword].append(uri)
                                 print(f"Added genre: {genre} for {artist_name}")
                             break
-                        
+
     # ...
+
 
 for song, details in songs.items():
     artist = details['artist']
     uri = details['uri']
-    search_for_genres(access_token, artist,uri)
-
+    search_for_genres(access_token, artist, uri)
 
 print("here they are")
 print(the_genres)
 
+# Create playlists for genres
 
 # In[47]:
+for genre, uris in the_genres.items():
+    user_id = "your user id"
 
-
-for genre, uris in the_genres.items(): 
-    user_id = "vasearcy"
-    
     # Create a playlist for the genre
     endpoint_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
     request_body = json.dumps({
@@ -236,39 +171,28 @@ for genre, uris in the_genres.items():
         "description": genre + " playlist",
         "public": False
     })
-    
+
     response = requests.post(url=endpoint_url, data=request_body, headers={
-                             "Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
+        "Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
     playlist_id = response.json()['id']
     print(f"Created playlist: {genre} - ID: {playlist_id}")
-    
+
     value_list = the_genres[genre]
     numberuris = len(value_list)
-    
+
     chunk_size = 99
     for i in range(0, numberuris, chunk_size):
         chunk_uris = value_list[i:i + chunk_size]
-    
-    # Add the list of track URIs to the playlist
+
+        # Add the list of track URIs to the playlist
         url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
         request_body = json.dumps({
             "uris": chunk_uris  # Pass the list of track URIs here
         })
 
         response = requests.post(url=url, data=request_body, headers={
-                             "Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
-    
-   #chunk_uris = value_list[-numberuris%chunk_size:]
+            "Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
 
-    # Add the list of track URIs to the playlist
-   # url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    #request_body = json.dumps({
-      #  "uris": chunk_uris  # Pass the list of track URIs here
-   # })
-
-    #response = requests.post(url=url, data=request_body, headers={
-                 #        "Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
-                         
     if response.status_code == 200:
         try:
             response_data = response.json()
@@ -278,126 +202,9 @@ for genre, uris in the_genres.items():
     else:
         print(f"Error: {response.status_code} - {response.text}")
 
-
-# In[11]:
-
-
-
-
-
-# In[36]:
-
-
-import json
-import matplotlib.pyplot as plt
-import datetime as dt
-import numpy as np
-import pandas as pd
-import os
-
-folder_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'Computer Science and Math', 'Spotify Project','MyData')
-#folder_path = '/home/Stella Fusaro/Documents/path/to/Computer Science and Math/Spotify Project/MyData'
-
-json_files =[f for f in os.listdir(folder_path)if f.endswith('.json')]
-
-all_data = pd.DataFrame()
-
-for json_file in json_files:
-    with open(os.path.join(folder_path,json_file), 'r')as file:
-        data = json.load(file)
-        temporary = pd.DataFrame(data)
-        all_data = pd.concat([all_data, temporary],ignore_index = True)
-
-all_data.drop(['username','platform','user_agent_decrypted','episode_name','episode_show_name','spotify_episode_uri','shuffle','offline','offline_timestamp','incognito_mode'],axis=1,inplace = True)
-#print(all_data)
-
-print("Shape of the DataFrame:", all_data.shape)
-n = 20
-test_data = all_data.head(n)
-
-songs = {}
-
-for index,row in test_data.iterrows():
-    song = row['master_metadata_track_name']
-    artist = row['master_metadata_album_artist_name']
-    uri = row['spotify_track_uri']
-    ts = [ts]
-    
-    if song and artist:
-        if(song,artist) not in songs.items():
-            songs[song] = {'artist': artist, "uri": uri, "ts": ts}
-        
-print(songs)
-print('hi')
-
-the_genres = {}
-artists_processed = set()
-
-def search_for_genres(token, artist_name,ts):
-    print(f"Searching for genres for {artist_name}")
-    url = "https://api.spotify.com/v1/search"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    query = f"q={artist_name}&type=artist&limit=1"
-
-    query_url = url + "?" + query
-    result = get(query_url, headers=headers)
-    json_result = result.json()
-    print(json_result)
-    artists = json_result.get("artists", {}).get("items", [])
-    for artist in artists:
-        if artist_name not in artists_processed:
-            genres = artist.get("genres", [])
-            if genres:
-                for genre in genres: 
-                    if genre in the_genres:
-                        print("always entered loop 1")
-                        the_genres[genre].append(ts)
-                    else:
-                        print("entered loop 2")
-                        the_genres[genre] = [ts]
-                    
-    # ...
-
-for song, details in songs.items():
-    artist = details['artist']
-    ts = details['ts']
-    search_for_genres(access_token, artist,ts)
-
-
-print("here they are")
-print(the_genres)
-
-data_frame = pd.DataFrame(data)
-print (data_frame)
-
-data_frame['ts'] = pd.to_datetime(df['ts'])
-
-df.set_index('ts', inplace = true)
-
-
-genre_counts = df.groupby('genre').resample('1H').count()['ts'].unstack().fillna(0)
-# Plot a stacked bar chart
-genre_counts.plot(kind='bar', stacked=True, figsize=(12, 6))
-
-# Customize the plot
-plt.title('Music Genre Distribution Over Time (1-hour intervals)')
-plt.xlabel('Time (1-hour intervals)')
-plt.ylabel('Number of Songs')
-plt.legend(title='Genre', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.xticks(rotation=45)
-
-# Show the plot
-plt.tight_layout()
-plt.show()
-
-
-
+# Save data to a CSV file
 
 # In[50]:
-
-
 data = []
 
 # Iterate through the_genres and create a list of dictionaries
@@ -408,10 +215,3 @@ for genre, uris in the_genres.items():
 # Create a DataFrame from the list of dictionaries
 dataframe = pd.DataFrame(data)
 dataframe.to_csv('the_genres.csv', index=False)
-
-
-# In[ ]:
-
-
-
-
